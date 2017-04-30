52fb47426a252e0f9a80254202e4d4a75851481a
Bolaji Olajide <bolaji.olajide@andela.com>
2017-04-30 23:31:36 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var express = require('express');
+var app = express();
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+var app = require('./app');
+var port = process.env.PORT;
+
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
