3897645364b6d6780a728a03af3eca56a5528727
Bolaji Olajide <bolaji.olajide@andela.com>
2017-05-01 12:22:34 +0100

    configure /users route

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var express = require('express');
 var app = express();
+var db = require('./db');
+
+var UserController = require('./user/UserController');
+app.use('/users', UserController);
+
 module.exports = app;
