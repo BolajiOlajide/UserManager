9ffea10398b7cd49037626afcdba7eacee42c2d9
Bolaji Olajide <bolaji.olajide@andela.com>
2017-05-01 12:20:04 +0100

    add user model and controller

---
+++ user/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+mongoose.model('User', UserSchema);
+
+module.exports = mongoose.model('User');
